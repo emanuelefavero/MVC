65fde87dad90c7da465298c32b773ab74b46db4a
emanuelefavero <emafaux89@gmail.com>
2022-10-17 08:15:36 +0200

    add 404 error handling

--- app.js
+++ app.js
@@ -37,25 +37,25 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/blog', blogRouter)
 
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+// Error handler
+app.use((err, req, res, next) => {
+  // Set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // Render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
-
-// Catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   next(createError(404))
-// })
-
-// Error handler
-// app.use((err, req, res, next) => {
-//   // Set locals, only providing error in development
-//   res.locals.message = err.message
-//   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-//   // Render the error page
-//   res.status(err.status || 500)
-//   res.render('error')
-// })
 
 module.exports = app
